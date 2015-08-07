4ae13c9d04f4ffc1e50af2cbcc5ade4b58d237eb
Steffen Schildknecht <steffen.schildknecht@zweitag.de>
2015-08-07 07:27:14 +0200

    Add basic messaging functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
+  before_action :require_current_user
+
   def index
+    @current_messages = Message.current.includes(:user).reverse
+  end
+
+  def create
+    current_user.messages.create(message_params)
+    redirect_to messages_path
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ActiveRecord::Base
   belongs_to :user
+
+  scope :current, -> { order(created_at: :desc).limit(5) }
 end
